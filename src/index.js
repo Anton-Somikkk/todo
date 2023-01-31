0dcac84edbb8531f4f004081d6c3c334a2fa5810
Anton-Book <kuvshinoffav@gmail.com>
2023-01-31 16:40:07 +0300

    444

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// eslint-disable-next-line import/no-unresolved
 import { Provider } from "react-redux";
 
 import App from "./App";
